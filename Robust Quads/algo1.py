d364a9ee7df15d7d867200fa44f68d1fb37e98b5
jonathanrjpereira <jonathanrjpereira@gmail.com>
2020-08-04 14:42:06 -0400

    Robust Quads Algo 1

---
+++ Robust Quads/algo1.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+
+from sklearn import manifold
+from sklearn.metrics import euclidean_distances
+
+import math
+
+from itertools import combinations  
+
+
+# Threshold Measurement Noise
+dmin = 0.0
+
+# Non - Flipped Nodes Example
+x_og_data = [0,0,2,2,28,10,100]
+y_og_data = [0,2,0,2,35,80,100]
+
+# Flipped Nodes Example
+# x_og_data = [0,0,2,2,100]
+# y_og_data = [0,2,0,2,100]
+
+
+# Total nuber of Anchor nodes
+node_count = len(x_og_data)
+
+# The Original (Global) Coordinates of the Anchor Nodes.
+og_coordinates = list(zip(x_og_data, y_og_data))
+print("Original Coordinates \n",og_coordinates)
+
+# Create a Distance Matrix from the Original Coordinates.
+og_distance_matrix = euclidean_distances(og_coordinates)
+print("Original Distance Matrix \n",og_distance_matrix)
+
+
+# Test if all test_nodes are already Robust
+def test_node_robustness(test_quad, robust_nodes):
+    result =  all(elem in robust_nodes  for elem in test_quad)
+    return result
+
+def get_triangle_angles(node1, node2, node3, distance_matrix):
+    # length of sides be a, b, c  
+    a = distance_matrix[node1, node2]  
+    b = distance_matrix[node1, node3]  
+    c = distance_matrix[node2, node3]
+
+    # print("a ",a)
+    # print("b ",b)
+    # print("c ",c)
+
+    # Square of lengths be a2, b2, c2  
+    a2 = pow(a, 2)  
+    b2 = pow(b, 2)  
+    c2 = pow(c, 2)
+
+    # From Cosine law
+    temp_alpha = round(((b2 + c2 - a2) / (2 * b * c)), 3)
+    temp_betta = round(((a2 + c2 - b2) / (2 * a * c)), 3)
+    temp_gamma = round(((a2 + b2 - c2) / (2 * a * b)), 3)   
+    alpha = math.acos(temp_alpha)  
+    betta = math.acos(temp_betta)  
+    gamma = math.acos(temp_gamma)  
+ 
+    # Converting to degree  
+    alpha = alpha * 180 / math.pi  
+    betta = betta * 180 / math.pi  
+    gamma = gamma * 180 / math.pi
+
+    # alpha = round(math.degrees(math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))))
+    # betta = round(math.degrees(math.acos((c ** 2 + a ** 2 - b ** 2) / (2 * c * a))))
+    # gamma = round(math.degrees(math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))))
+
+    angles = [alpha, betta, gamma]
+    sides = [a, b, c]
+    return angles, sides   
+
+def test_triangle_robustnes(node1, node2, node3, distance_matrix, dmin):
+    angles, sides = get_triangle_angles(node1, node2, node3, distance_matrix)
+    smallest_angle = min(angles)
+    shortest_side = min(sides)
+
+    if ((shortest_side) * (pow(math.sin(smallest_angle),2)) > dmin):
+        triangle_robustness = True
+    else:
+        triangle_robustness = False
+    return triangle_robustness
+
+
+# Test if a Quadilateral is robust
+def test_quad_robustness(test_quad, distance_matrix, dmin):
+    d012 = test_triangle_robustnes(test_quad[0], test_quad[1], test_quad[2], distance_matrix, dmin)
+    d013 = test_triangle_robustnes(test_quad[0], test_quad[1], test_quad[3], distance_matrix, dmin)
+    d023 = test_triangle_robustnes(test_quad[0], test_quad[2], test_quad[3], distance_matrix, dmin)
+    d123 = test_triangle_robustnes(test_quad[1], test_quad[2], test_quad[3], distance_matrix, dmin)
+
+    if d012 & d013 & d023 & d123:
+        quad_robustness = True
+    else:
+        quad_robustness = False
+    return quad_robustness
+
+
+# Node Name List Creation
+node_name = list(range(0, node_count))
+test_node_comb = list(combinations(node_name,4))
+test_quad_name = []
+for test_quad in test_node_comb:
+    if test_quad[0] == 0:
+        test_quad_name.append(test_quad)
+print(test_quad_name)
+
+# Create list of Robust Nodes and Robust Quads
+robust_nodes = []
+robust_quads = []
+
+for test_quad in test_quad_name:
+    if test_node_robustness(test_quad, robust_nodes) == True:
+        continue
+    else:
+        quad_robust_check = test_quad_robustness(test_quad, og_distance_matrix, dmin)
+
+    if quad_robust_check == True:
+        # Append the Robust Nodes
+        robust_nodes.extend(test_quad)
+        # Remove duplicate Robust Nodes
+        robust_nodes = list(set(robust_nodes)) 
+        # Append the Robust Quad
+        robust_quads.append(test_quad)
+
+print("Robust Nodes", robust_nodes)
+print("Robust Quads", robust_quads)
+
+
+# Plot the two graphs
+fig = plt.figure()
+
+# Original Coordinate Plot
+ax1 = fig.add_subplot(121)
+ax1.title.set_text('Original')
+og_coordinates = [list(ele) for ele in og_coordinates]
+og_coordinates = np.array(og_coordinates)
+# print(og_coordinates) 
+plt.scatter(og_coordinates[:, 0], og_coordinates[:, 1])
+
+ax2 = fig.add_subplot(122)
+ax2.title.set_text("Robust Node Quads")
+plt.scatter(og_coordinates[:, 0], og_coordinates[:, 1], c='red')
+plt.scatter(og_coordinates[robust_nodes, 0], og_coordinates[robust_nodes, 1], c='green')
+
+plt.show()
---
+++ Robust Quads/angoftri.py
@@ -0,0 +1,52 @@
+# Python3 code to find all three angles  
+# of a triangle given coordinate  
+# of all three vertices  
+import math 
+  
+# returns square of distance b/w two points  
+def lengthSquare(X, Y):  
+    xDiff = X[0] - Y[0]  
+    yDiff = X[1] - Y[1]  
+    return xDiff * xDiff + yDiff * yDiff 
+      
+def printAngle(A, B, C):  
+      
+    # Square of lengths be a2, b2, c2  
+    a2 = lengthSquare(B, C)  
+    b2 = lengthSquare(A, C)  
+    c2 = lengthSquare(A, B)  
+  
+    # length of sides be a, b, c  
+    a = math.sqrt(a2)  
+    b = math.sqrt(b2)  
+    c = math.sqrt(c2)  
+  
+    # From Cosine law  
+    alpha = math.acos((b2 + c2 - a2) /
+                         (2 * b * c))  
+    betta = math.acos((a2 + c2 - b2) / 
+                         (2 * a * c))  
+    gamma = math.acos((a2 + b2 - c2) / 
+                         (2 * a * b))  
+  
+    # Converting to degree  
+    alpha = alpha * 180 / math.pi  
+    betta = betta * 180 / math.pi  
+    gamma = gamma * 180 / math.pi 
+  
+    # printing all the angles  
+    # print("alpha : %f" %(alpha))  
+    # print("betta : %f" %(betta)) 
+    # print("gamma : %f" %(gamma)) 
+
+    angles = [alpha, betta, gamma]
+    return angles 
+
+
+
+# Driver code 
+A = (0, 0) 
+B = (0, 1)  
+C = (1, 0) 
+  
+printAngle(A, B, C)
